fdbdcaf3d41cedec10dbaafd3b72ecc549fa20ac
rami <rami.smat@hotmail.com>
2025-10-21 13:43:10 +0100

    removed some warnings and added more templates

--- src/main/java/com/vonoy/pdf_pipeline/api/dto/PdfJobRequest.java
+++ src/main/java/com/vonoy/pdf_pipeline/api/dto/PdfJobRequest.java
@@ -1,11 +1,9 @@
 package com.vonoy.pdf_pipeline.api.dto;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
 import java.util.Map;
 @Getter @Setter @AllArgsConstructor
 public class PdfJobRequest {
